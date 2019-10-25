59abe0820c0b52624845ff7caca35ece46b4fc05
Cui <ArsenYiJing.Cui@student.tdsb.on.ca>
2019-10-25 11:11:40 -0400

    Finishes 9

--- src/Question9.java
+++ src/Question9.java
@@ -1,9 +1,28 @@
 
 public class Question9 {
+	
+	public static double sin(int n, double x)
+	{
+		if (n == 1)
+			return Math.sin(x);
+		
+		else 
+			return Math.sin(x)*cos(n - 1, x) + Math.cos(x)*sin(n - 1, x);
+	}
+	
+	public static double cos(int n, double x)
+	{
+		if (n == 1)
+			return Math.cos(x);
+		
+		else
+			return Math.cos(x)*cos(n - 1, x) - Math.sin(x)*sin(n - 1, x);
+			
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println(sin(3, 1));
 	}
 
 }
